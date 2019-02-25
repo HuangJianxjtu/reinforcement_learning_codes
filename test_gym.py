e3d4fcbd34550190bc10adc4d48fa6ca6c01de08
Huang Jian <1479748954@qq.com>
2019-02-25 15:44:59 +0800

    Delete test_gym.py

--- test_gym.py
+++
@@ -1,8 +0,0 @@
-# test of the installation of gym
-
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render() # update the output (image)/图像引擎
-    env.step(env.action_space.sample()) # take a random action/物理引擎 
